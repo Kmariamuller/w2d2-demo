8e0c98195ec2e78c770115e8676642118f71f1d8
Katrina Muller <kmariamuller@gmail.com>
2016-09-20 01:32:40 +0000

    ruby day 1

---
+++ hello.rb
@@ -0,0 +1,11 @@
+#puts "hello, world!"
+
+mins_ago = 60
+
+if mins_ago > 60
+    puts "This happened over an hour ago."
+elsif mins_ago ==60
+    puts "This happened exactly one hour ago."
+else 
+    puts "this happened #{mins_ago} minutes ago."
+end
